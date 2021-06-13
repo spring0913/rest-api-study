d37557f27639b21294cc6c2acfc0132c1011056c
spring0913 <bbomi0913@gmail.com>
2021-06-13 18:43:20 +0900

    Event API 와 Test 코드 추가

---
+++ src/test/java/me/bmsung/demorestapi/events/EventControllerTests.java
@@ -0,0 +1,51 @@
+package me.bmsung.demorestapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("Study REST API with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021, 6, 13, 18, 00))
+                .endEventDateTime(LocalDateTime.of(2021, 6, 14, 18, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("서울특별시")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists()); // 201 Created
+    }
+}
