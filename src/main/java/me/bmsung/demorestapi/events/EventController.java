01d9a996b025a5cc15564fc5d18d7528a90a448d
spring0913 <bbomi0913@gmail.com>
2021-06-22 19:21:50 +0900

    스프링 HATEOAS 적용

--- src/main/java/me/bmsung/demorestapi/events/EventController.java
+++ src/main/java/me/bmsung/demorestapi/events/EventController.java
@@ -2,6 +2,7 @@ package me.bmsung.demorestapi.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -43,7 +44,8 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(event);
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
+        return ResponseEntity.created(createdUri).body(new EventResource(event));
     }
 }
---
+++ src/main/java/me/bmsung/demorestapi/events/EventResource.java
@@ -0,0 +1,24 @@
+package me.bmsung.demorestapi.events;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+public class EventResource extends RepresentationModel {
+    @JsonUnwrapped
+    private Event event;
+
+    public EventResource(Event event) {
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(event.getId());
+        add(linkTo(EventController.class).withRel("query-events"));
+        add(selfLinkBuilder.withSelfRel());
+        add(selfLinkBuilder.withRel("update-event"));
+        this.event = event;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+}
